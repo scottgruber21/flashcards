db2b903f53103ea50aba8f6f86a82c6620ef4333
Scott Gruber <scottgruber21@gmail.com>
2021-02-19 19:59:52 -0500

    initial upload

---
+++ index.js
@@ -0,0 +1,112 @@
+const submitButton = document.querySelector('#submitButton')
+const clock = document.querySelector('#clock')
+const timeSelect = document.querySelector('#timeSelect')
+const chord = document.querySelector('#chord')
+const timeDisplay = document.querySelector('#time-display')
+const addItem = document.querySelector('#addItem')
+const addItemForm = document.querySelector('#addItemForm')
+const listItems = document.querySelector('#listItems')
+const del = document.querySelector('#del')
+const clear = document.querySelector('#clear')
+const addMultipleForm = document.querySelector('#addMultipleForm')
+const addMultiple = document.querySelector('#addMultiple')
+
+let chordsList = localStorage.getItem('arr') ? JSON.parse(localStorage.getItem('arr')) : []
+
+let toDelete = []
+
+function rand(max){
+    return Math.floor(Math.random() * max)
+}
+
+timeSelect.addEventListener('input', function(){
+    document.querySelector('#time-display').innerHTML = timeSelect.value
+})
+
+
+function timer(){
+    let choice = chordsList.filter((item) => {
+        return item != chord.textContent
+    })
+    chord.innerText = choice[rand(choice.length)]
+}
+
+function pickList(){
+    let fullList = chordsList.map((item, index) => {
+    return `
+<input type="checkbox" id="item-${item}" name="item-${item}" value="${item}">
+<label for="item-${item}">${item}</label><br/>`
+}).join('')
+listItems.innerHTML = fullList
+localStorage.setItem('arr', JSON.stringify(chordsList))
+}
+
+addEventListener('load', function(){
+    pickList();
+})
+
+
+submitButton.addEventListener('click', function(e){
+    e.preventDefault();
+    chord.innerText = chordsList[rand(chordsList.length)]
+    countdown = setInterval(timer, timeSelect.value * 1000)
+    this.disabled = true
+    timeSelect.disabled = true
+}
+)
+
+ document.querySelector('#stop').addEventListener('click', function(){
+        clearInterval(countdown)
+        submitButton.disabled = false
+        timeSelect.disabled = false
+        chord.innerText = 'Click Start'
+    })
+
+addItemForm.addEventListener('submit', function(e){
+    e.preventDefault();
+    if(addItem.value){
+        chordsList.push(addItem.value)
+        pickList();
+        addItem.value=''
+    }
+})
+
+listItems.addEventListener('change', function(e){
+    if(e.target.checked){
+        toDelete.push(e.target.value)
+        console.log(toDelete)
+    }
+    else{
+        toDelete = toDelete.filter(item => item != e.target.value)
+        console.log(toDelete)
+    }
+    if(toDelete.length > 0){
+        del.disabled = false
+    }
+    else{
+        del.disabled = true
+    }
+   
+})
+
+del.addEventListener('click', function(){
+    chordsList = chordsList.filter((item) => !toDelete.includes(item)
+    )
+    toDelete = []
+    pickList();
+})
+
+clear.addEventListener('click', function(){
+    chordsList = []
+    pickList();
+})
+
+addMultipleForm.addEventListener('submit', function(e){
+    e.preventDefault();
+    if(addMultiple.value){
+        const temp = addMultiple.value.split(/,|\n/)
+        temp.forEach(item => chordsList.push(item))
+        pickList();
+        addMultiple.value=''
+    }
+})
